e67b384023b53317cd16b6874100a54135517aed
unknown <kingmmo824@gmail.com>
2025-06-09 15:08:03 +0100

    first commit

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from "$app/stores";
+	import Button from "$lib/shared/Button.svelte";
+</script>
+
+<section id="err-page" class="err-page">
+    <div>
+        <h1 class="font-bold text-3xl mb-3">{$page.status}: {$page.error.message}</h1>
+        <a href="/">
+            <Button>Go Home</Button>
+        </a>
+    </div>
+</section>
+
+<style>
+    .err-page {
+        margin: var(--gap) 0;
+    }
+</style>
