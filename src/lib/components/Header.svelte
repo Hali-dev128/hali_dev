8fc537299fdc021e19728e7cd122ccede0c54fa1
Hali-dev128 <hali.dev128@gmail.com>
2025-10-03 22:45:22 +0100

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,26 +1,34 @@
 <script>
-    $: theme = 'light';
+	$: theme = 'light';
 
-    const handleTheme = e => {
+    const handleTheme = () => {
         if (theme === 'dark') {
             theme = 'light';
             document.documentElement.style.setProperty('--sec-color', '#222d52');
             document.documentElement.style.setProperty('--bg-color', '#f9f6ef');
+            // @ts-ignore
             document.querySelector('nav').style.boxShadow = '4px 4px 5px #3332'
+            // @ts-ignore
+            document.querySelectorAll('.link').forEach(btn => btn.style.color = '#222d52');
+            // @ts-ignore
+            document.querySelectorAll('.currColor').forEach(btn => btn.style.color = '#222d52');
         } else {
             theme = 'dark';
-            document.documentElement.style.setProperty('--sec-color', '#f9f6ef');
-            document.documentElement.style.setProperty('--bg-color', '#222d52');
+            document.documentElement.style.setProperty('--sec-color', '#333');
+            document.documentElement.style.setProperty('--bg-color', '#000');
+            // @ts-ignore
             document.querySelector('nav').style.boxShadow = '2px 2px 8px #fff5'
+            // @ts-ignore
+            document.querySelectorAll('.bright').forEach(btn => btn.style.color = '#fff');
         }
     }
 </script>
 
 <header>
     <div>
-        <h1>Hali.dev</h1>
+        <h1 class="bright m">Hali.dev</h1>
 
-        <button class="cursor-pointer btn-active" on:click={handleTheme}>
+        <button class="bright cursor-pointer btn-active m" on:click={handleTheme}>
             {#if theme === 'dark'}
                 <svg data-v-14c8c335="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun lucide-icon customizable"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></svg>
                 {:else if theme === 'light'}
