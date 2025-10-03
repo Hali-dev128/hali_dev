49be112a9b313bac07b07fe1c543f8f90ddd4bd4
Hali-dev128 <hali.dev128@gmail.com>
2025-10-03 22:51:07 +0100

    Update Services.svelte

--- src/lib/pages/Services.svelte
+++ src/lib/pages/Services.svelte
@@ -7,32 +7,32 @@
 </script>
 
 <section  id="Services">
-    <h2 class="text-3xl font-bold text-center">My Services</h2>
+    <h2 class="text-3xl font-bold text-center bright currColor">My Services</h2>
     <ul class="services">
-       <li>
+       <li class="bright toScrollAnimate">
         <img src={Maintenance} alt="Maintenance Icon">
         <h3 class="font-bold">Website Fixes and Maintenance</h3>
         <p>I can help you fix layout problems, broken links, and bugs. I also offer ongoing support to update content, manage backups, and improve your website without starting from scratch.</p>
        </li>
-       <li>
+       <li class="bright toScrollAnimate">
         <img src={Responsive} alt="Responsive Icon">
         <h3 class="font-bold">Responsive Design</h3>
         <p>I design every website to automatically adjust to all screen sizes, making sure your users have the best experience no matter what device they use.</p>
        </li>
-       <li>
+       <li class="bright toScrollAnimate">
         <img src={Code} alt="Code Icon">
         <h3 class="font-bold">Clean Code</h3>
-        <p>I write well-structured, semantic code that,s easy to understand and maintain. This makes future updates faster, cheaper, and less stressful for both of us.</p>
+        <p>I write well-structured, semantic code that's easy to understand and maintain. This makes future updates faster, cheaper, and less stressful for both of us.</p>
        </li>
-       <li>
+       <li class="bright toScrollAnimate">
         <img src={SEOIcon} alt="SEO Icon">
         <h3 class="font-bold">SEO Optimization</h3>
         <p>I use proper HTML structure, meta tags, alt text, and performance best practices to make sure your website is visible to search engines like Google.</p>
        </li>
-       <li>
+       <li class="bright toScrollAnimate">
         <img src={Response} alt="Response Icon">
         <h3 class="font-bold">Fast Response</h3>
-        <p>I optimize images, minimize code, and follow best practices to ensure your site loads quickly. A fast website keeps visitors engaged and improves SEO.</p>
+        <p>I value your time. That’s why I respond to client messages, questions, and requests quickly and on time. Whether you need project updates, minor changes, or answers to your concerns, you can always count on me to stay available and keep communication smooth.</p>
        </li>
     </ul>
 </section>
@@ -63,6 +63,8 @@
         display: flex;
         gap: 5px;
         flex-direction: column;
+        border-radius: 10px;
+        box-shadow: var(--set-shadow);
         justify-content: center;
         align-items: center;
         text-align: center;
