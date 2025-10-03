dcf99e2c4d4fd245bea533d79550be707df03838
Hali-dev128 <hali.dev128@gmail.com>
2025-10-03 22:52:04 +0100

    Update Projects.svelte

--- src/lib/pages/Projects.svelte
+++ src/lib/pages/Projects.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <section id="Projects">
-    <h2 class="text-3xl font-bold text-center">Projects</h2>
+    <h2 class="text-3xl font-bold text-center bright currColor">Projects</h2>
 
     <ul>
-        <li>
+        <li class="bright toScrollAnimate">
             <img src={ExpenseTracker} alt="Expense Tracker Screenshot">
             <div>
                 <h3 class="font-bold">HomeBudget - Personal Expense Tracker</h3>
@@ -16,11 +16,11 @@
                 <p><strong>Built with:</strong> HTML, CSS, Sveltekit</p>
                 <p><strong>Features:</strong> Dynamic input, localStorage saving, responsive layout</p>
             </div>
-            <a href="../" target="_blank" aria-label="View code" title="View code">
+            <a href="https://github.com/Hali-dev128/Expense-tracker.git" class="bright currColor" target="_blank" aria-label="View code" title="View code">
                 <svg data-v-14c8c335="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github lucide-icon customizable"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
             </a>
         </li>
-        <li>
+        <li class="bright toScrollAnimate">
             <img src={Blog} alt="Food History Blog Screenshot">
             <div>
                 <h3 class="font-bold">Snack to the Past - Food History Blog</h3>
@@ -28,11 +28,11 @@
                 <p><strong>Built with:</strong> HTML, CSS, Sveltekit</p>
                 <p><strong>Features:</strong> Article layout, search filter, mobile-first design</p>
             </div>
-            <a href="../" target="_blank" aria-label="View code" title="View code">
+            <a href="https://github.com/Hali-dev128/History-Food-Blog.git" class="bright currColor" target="_blank" aria-label="View code" title="View code">
                 <svg data-v-14c8c335="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github lucide-icon customizable"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
             </a>
         </li>
-        <li>
+        <li class="bright toScrollAnimate">
             <img src={Mart} alt="BingMart Screenshot">
             <div>
                 <h3 class="font-bold">BingMart - Advertising Site for Online Store</h3>
@@ -40,7 +40,7 @@
                 <p><strong>Built with:</strong> HTML, CSS</p>
                 <p><strong>Features:</strong> Product cards, responsive grid, call-to-action sections</p>
             </div>
-            <a href="../" target="_blank" aria-label="View code" title="View code">
+            <a href="https://github.com/Hali-dev128/BingMart.git" class="bright currColor" target="_blank" aria-label="View code" title="View code">
                 <svg data-v-14c8c335="" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github lucide-icon customizable"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
             </a>
         </li>
@@ -68,6 +68,8 @@
         max-width: 350px;
         display: flex;
         flex-direction: column;
+        box-shadow: var(--set-shadow);
+        border-radius: 10px;
         gap: 15px;
         padding: 10px;
         background-color: var(--sec-color);
