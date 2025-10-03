a4f879ce47711d86e6a934fc5ee9596379b4da4d
Hali-dev128 <hali.dev128@gmail.com>
2025-10-03 22:48:57 +0100

    Update About.svelte

--- src/lib/pages/About.svelte
+++ src/lib/pages/About.svelte
@@ -7,28 +7,28 @@
 </script>
 
 <section id="About">
-    <h2 class="text-3xl font-bold text-center">About Me</h2>
+    <h2 class="text-3xl font-bold text-center bright currColor">About Me</h2>
     <div class="about">
-        <img src={AboutMe} alt="Pic of a keyboard with a stairway">
+        <img src={AboutMe} class="toScrollAnimate" alt="Pic of a keyboard with a stairway">
         <div class="details">
-            <span>
-                <p>Hi, I'm Odumu Muhammad Halidu the mind behind Hali Tech. I'm a Computer Science student passionate about building fast, mobile-friendly websites using HTML, CSS, Javascript and Sveltekit.</p>
-                <p>I specialize in helping small businesses and student entrepreneurs create an online presence even if they're starting from scratch.</p>
+            <span class="bright currColor">
+                <p>Hi, I'm Odumu Muhammad Halidu the mind behind Hali.dev. I'm a Web Developer builds fast, mobile-friendly websites using HTML, CSS, Javascript and Sveltekit.</p>
+                <p>I specialize in helping businesses create an online presence even if they're starting from scratch.</p>
             </span>
             <ul>
-                <li>
+                <li class="bright toScrollAnimate currColor">
                     <img src={HTMLIcon} alt="HTML Icon">
                     <p>HTML</p>
                 </li>
-                <li>
+                <li class="bright toScrollAnimate currColor">
                     <img src={CSSIcon} alt="CSS Icon">
                     <p>CSS</p>
                 </li>
-                <li>
+                <li class="bright toScrollAnimate currColor">
                     <img src={Javascript} alt="Javascript Icon">
                     <p>Javascript</p>
                 </li>
-                <li>
+                <li class="bright toScrollAnimate currColor">
                     <img src={Sveltekit} alt="Sveltekit Icon">
                     <p>Sveltekit</p>
                 </li>
@@ -56,6 +56,8 @@
     }
 
     .about > img {
+        border-radius: 10px;
+        box-shadow: var(--set-shadow);
         width: max(280px, 33%);
         max-height: 450px;
     }
@@ -79,6 +81,8 @@
     .details ul {
         display: flex;
         flex-wrap: wrap;
+        box-shadow: var(--set-shadow);
+        border-radius: 10px;
         background-color: var(--sec-color);
         padding: 10px;
         gap: 10px;
@@ -91,6 +95,7 @@
         gap: 5px;
         font-weight: bold;
         background-color: var(--bg-color);
+        border-radius: 10px;
     }
 
     @media (max-width: 980px) {
